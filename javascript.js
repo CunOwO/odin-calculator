1445b1bc86a0cc4ee529b49c407b2c7e3de7c4f8
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-25 16:25:57 +0700

    Implement basic calculator operations and update display functionality

--- javascript.js
+++ javascript.js
@@ -1,33 +1,64 @@
 const numBtn = document.querySelectorAll(".num-btn");
-// const addBtn = 
 const span = document.querySelector("span");
+const buttons = document.querySelector(".button-container");
+let currentNumber = 0;
+let currentResult = 0;
+let input = "";
+let previousOperation = null;
 
-let operand1 ="";
-let operand2 ="";
-numBtn.forEach((button) => {
-    button.addEventListener("click", () => {
-        operand1 += button.textContent;
-        span.textContent = operand1;
-        if (!span.style.fontSize) {
-            span.style.fontSize = window.getComputedStyle(span).fontSize;
-        }
-        if (span.offsetWidth > 297) {
-            span.style.fontSize = (parseInt(span.style.fontSize) * 0.9) + "px";
-        }
-    });
+numBtn.forEach((button) => button.addEventListener("click", updateScreen));
+
+function updateScreen(e) {
+    input += e.target.textContent;
+    span.textContent = input;
+    
+    if (!span.style.fontSize) {
+        span.style.fontSize = window.getComputedStyle(span).fontSize;
+    }
+    if (span.offsetWidth > 297) {
+        span.style.fontSize = (parseInt(span.style.fontSize) * 0.9) + "px";
+    }
+}
+
+buttons.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target.classList.contains("btn-add")) {
+        handleOperation("add");
+    }
+    else if (target.classList.contains("btn-subtract")) {
+        handleOperation("subtract");
+    }
+    else if (target.classList.contains("btn-multiply")) {
+        handleOperation("multiply");
+    }
+    else if (target.classList.contains("btn-divide")) {
+        handleOperation("divide");
+    }
 });
-function add(operand1, operand2) {
-    return operand1 + operand2;
+
+function handleOperation(operation) {
+    if (input !== "") {
+        currentNumber = Number(input);
+        currentResult = calculate(currentResult, currentNumber, previousOperation);
+        
+        span.textContent = currentResult;
+        input = "";
+        previousOperation = operation;
+    }
 }
 
-function substract(operand1, operand2) {
-    return operand1 - operand2;
+function calculate(operand1, operand2, operation) {
+    switch(operation) {
+        case "add":
+            return operand1 + operand2;
+        case "subtract":
+            return operand1 - operand2;
+        case "multiply":
+            return operand1 * operand2;
+        case "divide":
+            return operand1 / operand2;
+        default:
+            return operand2;
+    }
 }
 
-function multiply(operand1, operand2) {
-    return operand1 * operand2;
-}
-
-function divide(operand1, operand2) {
-    return operand1 / operand2;
-}
